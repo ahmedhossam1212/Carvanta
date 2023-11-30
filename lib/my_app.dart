065189b887e4752fe9406f5f1b7e09728043286e
ahmedhossam1212 <pmedo1689@gmail.com>
2023-11-30 23:34:12 +0200

    create form_field

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,6 @@
+import 'package:carvanta/core/utils/app_colors.dart';
+import 'package:carvanta/presentation/view/widgets/input_field.dart';
+import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -5,6 +8,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: Tester(),
+    );
+  }
+}
+
+class Tester extends StatelessWidget {
+  Tester({super.key});
+  final TextEditingController textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.grey,
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(children: [
+          defualtFormField(context,
+              controller: textEditingController,
+              validate: () {},
+              hint: "hint",
+              type: TextInputType.name,
+              suffix: IconButton(
+                  onPressed: () {}, icon: Icon(EneftyIcons.arrow_left_3_bold))),
+        ]),
+      ),
+    );
   }
 }
---
+++ lib/presentation/view/widgets/input_field.dart
@@ -0,0 +1,39 @@
+import 'package:carvanta/core/utils/app_colors.dart';
+import 'package:carvanta/core/utils/style_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:carvanta/core/utils/media_query_values.dart';
+
+Widget defualtFormField(
+  BuildContext context, {
+  required TextEditingController controller,
+  required Function validate,
+  required String hint,
+  required TextInputType type,
+  bool isPassword = false,
+  Widget? suffix,
+}) =>
+    Container(
+      width: double.infinity,
+      height: context.height * 0.06,
+      decoration: BoxDecoration(
+          color: AppColors.white, borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          obscureText: isPassword,
+          keyboardType: type,
+          controller: controller,
+          validator: (String? s) {
+            return validate(s);
+          },
+          cursorColor: AppColors.lightBlack,
+          style: getMediumStyle(color: AppColors.lightBlack, fontSize: 17),
+          decoration: InputDecoration(
+              hintStyle:
+                  getMediumStyle(color: AppColors.lightBlack, fontSize: 17),
+              hintText: hint,
+              suffixIcon: suffix,
+              border: InputBorder.none),
+        ),
+      ),
+    );
