70dbdaf6e3bdc79b9ca992fcb0237071510fc723
ahmedhossam1212 <pmedo1689@gmail.com>
2023-12-27 17:18:13 +0200

    create ChangePassword

--- lib/presentation/view/screens/change_password_screen.dart
+++ lib/presentation/view/screens/change_password_screen.dart
@@ -1,5 +1,8 @@
 import 'package:carvanta/core/utils/app_colors.dart';
+import 'package:carvanta/core/utils/media_query_values.dart';
 import 'package:carvanta/core/utils/style_manager.dart';
+import 'package:carvanta/presentation/view/widgets/input_field.dart';
+import 'package:carvanta/presentation/view/widgets/main_button.dart';
 import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +14,10 @@ class ChangePasswordScreen extends StatefulWidget {
 }
 
 class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  TextEditingController oldPassController = TextEditingController();
+  TextEditingController newPassController = TextEditingController();
+  TextEditingController confirmPassController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +36,61 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
           style: getRegularStyle(color: AppColors.black, fontSize: 23),
         ),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Old Password",
+                style: getRegularStyle(
+                    color: AppColors.black, fontSize: context.height * 0.025),
+              ),
+              defualtFormField(context,
+                  controller: oldPassController,
+                  validate: () {},
+                  hint: "",
+                  type: TextInputType.text,
+                  width: double.infinity),
+              SizedBox(
+                height: context.height * 0.01,
+              ),
+              Text(
+                "New Password",
+                style: getRegularStyle(
+                    color: AppColors.black, fontSize: context.height * 0.025),
+              ),
+              defualtFormField(context,
+                  controller: newPassController,
+                  validate: () {},
+                  hint: "",
+                  type: TextInputType.text,
+                  width: double.infinity),
+              SizedBox(
+                height: context.height * 0.01,
+              ),
+              Text(
+                "Confirm New Password",
+                style: getRegularStyle(
+                    color: AppColors.black, fontSize: context.height * 0.025),
+              ),
+              defualtFormField(context,
+                  controller: confirmPassController,
+                  validate: () {},
+                  hint: "",
+                  type: TextInputType.text,
+                  width: double.infinity),
+              SizedBox(
+                height: context.height * 0.2,
+              ),
+              mainButton(context,
+                  onpressd: () {}, background: AppColors.red, text: "Save")
+            ],
+          ),
+        ),
       ),
     );
   }
